308749c2fe51e11e99f60bfe756eeaa6be169c68
jc <avaz28082000@gmail.com>
2020-06-05 09:47:25 +0500

    Type in UserContext

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -2,7 +2,7 @@ import { createContext } from 'react';
 
 const UserContext = createContext({
     isAuthenticated: false,
-    setIsAuthenticated: true,
+    setIsAuthenticated: () => {},
 });
 
 export default UserContext;
